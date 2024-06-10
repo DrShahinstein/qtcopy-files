e5262d73232a6ac5d99922482e669a9c4a969f28
DrShahinstein <1taylanozturk@protonmail.com>
2024-06-10 23:12:13 +0300

    Create new slot to handle triggers on QListWidget

--- include/dialogwindow.h
+++ include/dialogwindow.h
@@ -17,6 +17,7 @@ protected:
 private slots:
   void get_target_dir();
   void move_files();
+  void onCustomContextMenuRequested(const QPoint &pos); // to delete QListWidgetItem when triggered
 
 private:
   QStringList incoming_files;
