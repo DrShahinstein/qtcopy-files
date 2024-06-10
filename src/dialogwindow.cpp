e089daa420a8b5b9ddc66f22930be5a65ed03af5
DrShahinstein <1taylanozturk@protonmail.com>
2024-06-10 23:13:50 +0300

    Consider possible bugs and improve the program features

--- src/dialogwindow.cpp
+++ src/dialogwindow.cpp
@@ -5,11 +5,15 @@
 #include <QFile>
 #include <QFileDialog>
 #include <QFileInfo>
+#include <QListWidget>
 #include <QListWidgetItem>
 #include <QMessageBox>
 #include <QMimeData>
 #include <QString>
 #include <QtCore/Qt>
+#include <QMenu>
+#include <QAction>
+#include <QPoint>
 
 QString get_current_target_dir() {
   QString target_dir;
@@ -40,6 +44,18 @@ void copy_path(QString src, QString dst) {
   }
 }
 
+void clear_files_view(QListWidget *files_view) {
+#ifdef Q_OS_WIN
+  // Windows-specific clearing (windows sucks)
+  while (files_view->count() > 0) {
+    files_view->takeItem(0);
+  }
+#else
+  // This is veeeerry interestingly causing crash in windows
+  files_view->clear();
+#endif
+}
+
 DialogWindow::DialogWindow(QWidget *parent) : QDialog(parent) {
   setupUi(this);
 
@@ -49,30 +65,29 @@ DialogWindow::DialogWindow(QWidget *parent) : QDialog(parent) {
   drag_n_drop_area->installEventFilter(this);
   btn_select_target_dir->setText(get_current_target_dir());
   btn_select_target_dir->setCursor(Qt::CursorShape::PointingHandCursor);
+  files_view->setStyleSheet(LISTWIDGET_QSS);
+  files_view->setContextMenuPolicy(Qt::CustomContextMenu);
 
   connect(btn_select_target_dir, &QPushButton::clicked, this, &DialogWindow::get_target_dir);
   connect(btn_submit, &QPushButton::clicked, this, &DialogWindow::move_files);
-
-  files_view->setStyleSheet(LISTWIDGET_QSS);
+  connect(files_view, &QListWidget::customContextMenuRequested, this, &DialogWindow::onCustomContextMenuRequested);
 }
 
 void DialogWindow::get_target_dir() {
   QString target_path = QFileDialog::getExistingDirectory(
       this, "Select Directory", QDir::currentPath());
 
-  if (!target_path.isEmpty()) {
-    if (btn_select_target_dir) {
-      btn_select_target_dir->setText(target_path);
-    }
+  if (!target_path.isEmpty() && btn_select_target_dir) {
+    btn_select_target_dir->setText(target_path);
+  }
 
-    QFile file(TARGET_DIR_PATH);
-    if (file.open(QIODevice::WriteOnly | QIODevice::Text)) {
-      QTextStream out(&file);
-      out << target_path;
-      file.close();
-    } else {
-      qWarning() << "Error writing to file:" << TARGET_DIR_PATH;
-    }
+  QFile file(TARGET_DIR_PATH);
+  if (file.open(QIODevice::WriteOnly | QIODevice::Text)) {
+    QTextStream out(&file);
+    out << target_path;
+    file.close();
+  } else {
+    qWarning() << "Error writing to file:" << TARGET_DIR_PATH;
   }
 }
 
@@ -85,12 +100,8 @@ void DialogWindow::move_files() {
     return;
   }
 
-  QString wrapping_dir_name = lineedit_target_dir->text().isEmpty()
-                                  ? DEFAULT_WRAPPING_DIR
-                                  : lineedit_target_dir->text();
-
-  QString wrapping_dir_path =
-      target_dir + QDir::separator() + wrapping_dir_name;
+  QString wrapping_dir_name = lineedit_target_dir->text().isEmpty() ? DEFAULT_WRAPPING_DIR : lineedit_target_dir->text();
+  QString wrapping_dir_path = target_dir + QDir::separator() + wrapping_dir_name;
   QDir wrdir(wrapping_dir_path);
   if (!wrdir.exists()) {
     wrdir.mkdir(wrapping_dir_path);
@@ -100,6 +111,18 @@ void DialogWindow::move_files() {
     QFileInfo file_info(f);
     QString fname = file_info.fileName();
     QString fdest = wrapping_dir_path + QDir::separator() + fname;
+
+    if (file_info.isDir() && fname == wrapping_dir_name) {
+      QString error_message =
+          QString(
+              "The directory '%1' has the same name as the wrapping directory. "
+              "This would create a recursive directory structure. Aborted.")
+              .arg(file_info.fileName());
+      QMessageBox::critical(this, "Error", error_message);
+      incoming_files.clear();
+      clear_files_view(files_view);
+      return;
+    }
 
     try {
       if (file_info.isDir()) {
@@ -118,7 +141,23 @@ void DialogWindow::move_files() {
   }
 
   incoming_files.clear();
-  files_view->clear();
+  clear_files_view(files_view);
+}
+
+void DialogWindow::onCustomContextMenuRequested(const QPoint &pos) {
+  QListWidgetItem *item = files_view->itemAt(pos);
+  if (item) {
+    QMenu contextMenu(this);
+    QAction *deleteAction = contextMenu.addAction("Delete");
+
+    connect(deleteAction, &QAction::triggered, [this, item]() {
+      int row = files_view->row(item);
+      incoming_files.clear();
+      files_view->takeItem(row);
+    });
+
+    contextMenu.exec(files_view->mapToGlobal(pos));
+  }
 }
 
 bool DialogWindow::eventFilter(QObject *obj, QEvent *event) {
